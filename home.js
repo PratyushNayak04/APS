29f890a767e1fc93e2a188133fccbf0a38953eab
PratyushNayak04 <147187232+PratyushNayak04@users.noreply.github.com>
2024-07-13 14:44:49 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    var navbar = document.getElementById("navbar");
+    var scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
+    if (scrollPosition >= 80) {
+        navbar.style.backgroundColor = "rgba(0, 0, 0, 0.55)"; 
+    } else {
+        navbar.style.backgroundColor = "rgba(0, 0, 0, 0)"; 
+    }
+}
+
+function clickFunction(){
+    alert("Working") ; 
+}
+
